2a1e66a7e38130cdb96ebaf23e781e930402ed8c
Luke K <lukekaicher00@gmail.com>
2023-07-27 14:35:15 -0400

    removed code for button in guitoolbar

--- Assets/Game/Gui/Toolbar/GuiToolbar.cs
+++ Assets/Game/Gui/Toolbar/GuiToolbar.cs
@@ -8,24 +8,4 @@ public class GuiToolbar : GuiScript<GuiToolbar>
 {
 
 
-	IEnumerator OnClickNewButtonText( IGuiControl control )
-	{
-		Settings.LanguageId = (Settings.LanguageId + 1) % Settings.GetLanguages().Length;
-		
-		
-		yield return E.Break;
-	}
-
-	IEnumerator OnClickChangePhaseButton( IGuiControl control )
-	{
-		Settings.LanguageId = (1 - (Settings.LanguageId - 1)) + 1;
-		
-		Globals.SetPhase(Settings.LanguageId);
-		
-		
-		
-		// ChangePhaseButton.Text = "Hello";
-		
-		yield return E.Break;
-	}
 }
