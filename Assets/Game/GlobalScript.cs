8cae44e5bb2635cfe1556db716aad72b5e08091f
Luke Kaicher <luke.kaicher@stonybrook.edu>
2024-04-19 21:44:48 -0400

    disable repairkit at start

--- Assets/Game/GlobalScript.cs
+++ Assets/Game/GlobalScript.cs
@@ -81,7 +81,7 @@ public partial class GlobalScript : GlobalScriptBase<GlobalScript>
 		
 		
 		// temporary
-		I.RepairKit.Add();
+		// I.RepairKit.Add();
 		
 		// I.BilgePump.Add();
 		 // C.Tony.Enable();
